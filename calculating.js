d8d0b3993bf68288e8e76688662e0e30b3b08a00
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2022-11-12 17:54:41 +0000

    Create constants for carbon footprint calculation

---
+++ calculating.js
@@ -0,0 +1,10 @@
+const carbonIntensityFactor = {
+'europeanUnion': 276,
+'france': 34.8,
+'unitedStates': 493,
+'china': 681,
+'other': 519};
+
+const kWhPerByteDataCenter = 0.000000000072;
+const kWhPerByteNetwork = 0.000000000152;
+const kWhPerMinuteDevice = 0.00021;
