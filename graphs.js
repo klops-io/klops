d3421902e80ff6f3fb9fb68916f232808f9c8786
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2022-11-12 21:34:09 +0000

    create headers

---
+++ graphs.js
@@ -0,0 +1,13 @@
+JSC.Chart('past', {
+    type: 'horizontal column',
+    series: [
+       {
+          points: [
+             {x: 'Apples', y: 50},
+             {x: 'Oranges', y: 42}
+          ]
+       }
+    ]
+ });
+JSC.Chart('region', {});
+JSC.Chart('world', {});
