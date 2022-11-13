a69d510e07caddeae921081cf1fd9f1c80de093e
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2022-11-13 11:26:37 +0000

    some changes

--- calc.js
+++ calc.js
@@ -1,3 +1,8 @@
+$(document).mousemove(function(e){
+    $("#image").stop().animate({left:e.pageX, top:e.pageY});
+});
+
+
 const carbonIntensityFactorIngCO2PerKWh = {
     'EU': 276,
     'France': 34.8,
--- script.js
+++ script.js
@@ -24,11 +24,6 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 */
-
-$(document).mousemove(function(e){
-    $("#image").stop().animate({left:e.pageX, top:e.pageY});
-});
-
 extractHostname = (url) => {
     let hostname = url.indexOf("//") > -1 ? url.split('/')[2] : url.split('/')[0];
   
