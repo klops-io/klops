b622079430194da6a5340ddf03e4ff7a266f8b69
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2022-11-13 03:32:14 +0000

    add constant values

--- button.js
+++ button.js
@@ -1,3 +1,14 @@
+const carbonIntensityFactor = {
+    'europeanUnion': 276,
+    'france': 34.8,
+    'unitedStates': 493,
+    'china': 681,
+    'other': 519};
+    
+const kWhPerByteDataCenter = 0.000000000072;
+const kWhPerByteNetwork = 0.000000000152;
+const kWhPerMinuteDevice = 0.00021;
+
 const start_button = document.querySelector('#start');
 const stop_button = document.querySelector('#stop');
 
